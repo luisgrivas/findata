d0b603f20940d7372610a377c7c3550c7ec09219
Luis Gonzalez <luisgzzrivas@gmail.com>
2018-07-16 19:09:32 -0500

    Modificado download.py para que descargue precios de todos los tickers en nasdaq y nyse

--- code/download.py
+++ code/download.py
@@ -3,21 +3,19 @@ import pandas as pd
 import pandas_datareader as pdr
 import datetime 
 
-sample_size = 10
-
 nyse_tickers = pd.read_csv('data/companylist_nyse.csv', encoding='latin1')
 nyse_tickers = nyse_tickers.loc[nyse_tickers.MarketCap != 0, ]
 
 nasdaq_tickers = pd.read_csv('data/companylist_nasdaq.csv', encoding='latin1')
 nasdaq_tickers = nasdaq_tickers.loc[nasdaq_tickers.MarketCap != 0, ]
 
-sample1 = nyse_tickers.sample(sample_size)['Symbol'].tolist()
-sample2 = nasdaq_tickers.sample(sample_size)['Symbol'].tolist()
+list1= nyse_tickers['Symbol'].str.strip().tolist()
+list2 = nasdaq_tickers['Symbol'].str.strip().tolist()
 
-sample = sample1 + sample2
+tickers = list1 + list2
 
-for ticker in sample:
+for ticker in tickers:
 	df = pdr.get_data_yahoo(ticker,
-		start=datetime.datetime(2010, 1, 1),
+		start=datetime.datetime(1950, 1, 1),
 		end=datetime.datetime(2018, 7, 16))
 	df.to_csv('data/stocks/' + ticker + '.csv')
